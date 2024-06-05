ef0f97e5a32b59455615f12d6ab7a73d6d105f5e
Grischa <Grischa.Taenzer@gmail.com>
2024-06-05 22:49:49 +0200

    Learn firebase

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,17 +1,50 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, doc, onSnapshot, addDoc, updateDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NoteListService {
+  normalNotes: Note[] = [];
+  trashNotes: Note[] = [];
 
   firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  unsubNotes;
+  unsubTrash;
 
-  getNormalRef() {
+  constructor() {
+    this.unsubNotes = this.subNotesList();
+    this.unsubTrash = this.subTrashList();
+  }
+
+  ngonDestroy() {
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  getNotesRef() {
     return collection(this.firestore, 'notes');
   }
 
@@ -19,7 +52,29 @@ export class NoteListService {
     return collection(this.firestore, 'trash');
   }
 
-  getSingleDocRef(colId:string, docId:string) {
+  getSingleDocRef(colId: string, docId: string) {
     return doc(collection(this.firestore, colId), docId);
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || 'note',
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false,
+    }
+  }
+
+  async addNote(item: Note) {
+    const docRef = await addDoc(this.getNotesRef(), item).catch(
+      (err) => { console.error(err) }
+    ).then(
+      (docRef) => { console.log('Doc ID:', docRef?.id); }
+    )
+  }
+
+  async updateNote(colId: string, docId: string, item: {}) {
+    await updateDoc(this.getSingleDocRef(colId, docId), item);
   }
 }
